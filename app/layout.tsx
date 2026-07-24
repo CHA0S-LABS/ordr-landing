d95c8cf8fb52432aa72538409c8aead33b062ca3
AvhiMaz <avhimazumder5@outlook.com>
2026-07-24 08:57:11 +0530

    feat: add favicon
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,11 @@ export const metadata: Metadata = {
   title: "ordr.trade",
   description:
     "A fully on chain order book exchange on Solana that gives market makers their own private accounts, cheap repricing, and protection from toxic arbitrage.",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
